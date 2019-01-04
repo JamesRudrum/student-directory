6e643508d5322908190e7dd1ef3c86b24cac2ad0
James <james.rudrum@hyperisland.se>
2019-01-04 21:44:51 +0000

    Step8 Ex8 & Ex9

--- directory.rb
+++ directory.rb
@@ -51,7 +51,12 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  student_count = students.student_count
+  if student_count == 1
+    puts "Overall, we have #{student_count} great student."
+  else
+    puts "Overall, we have #{student_count} great students."
+  end
 end
 students = input_students
 print_header
