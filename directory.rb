443c35c9d2c009ec017c1c9e1fb1e9073fb38fba
James <james.rudrum@hyperisland.se>
2019-01-04 19:32:27 +0000

    step8 ex3

--- directory.rb
+++ directory.rb
@@ -31,7 +31,7 @@ end
 def print(students)
   filter = specific_letter
   students.each_with_index do |student, index|
-    if student[:name].start_with?(filter)
+    if student[:name].start_with?(filter) && student[:name].length <= 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
     end
   end
