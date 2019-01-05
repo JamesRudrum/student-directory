b7ab5f7773669f425143f078aa9c896ba2b9805c
James <james.rudrum@hyperisland.se>
2019-01-05 01:52:21 +0000

    Step 10 - refactoring, adding instance variable

--- directory.rb
+++ directory.rb
@@ -1,11 +1,17 @@
+@students = []
+
 def replace_chmp(string)
   return string.rstrip
+end
+
+def order_students
+  @students.sort_by! { |hsh| hsh[:cohort] }
 end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  students = []
+  @students = []
   cohorts = ["January","February","March","April","May","June","July","August","September","October","November","December"]
   name = gets
   name = replace_chmp(name)
@@ -21,12 +27,12 @@ def input_students
       cohort = "November"
     end
 
-    students << {name: name, cohort: cohort.to_sym, hobbies: hobbies}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies}
+    puts "Now we have #{@students.count} students"
     name = gets
     name = replace_chmp(name)
   end
-  students
+  order_students
 end
 
 def specific_letter
@@ -46,24 +52,24 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
+def print_students_list
   filter = specific_letter
-  count = students.count
+  count = @students.count
   if count < 1 || nil
     puts "Please add a student"
     input_students
   end
   i = 0
   while i < count
-    if students[i][:name].start_with?(filter) && students[i][:name].length <= 12
-      puts "#{i + 1}: Name: #{students[i][:name]} (Cohort: #{students[i][:cohort]}) (Hobbies: #{students[i][:hobbies]})".center(80)
+    if @students[i][:name].start_with?(filter) && @students[i][:name].length <= 12
+      puts "#{i + 1}: Name: #{@students[i][:name]} (Cohort: #{@students[i][:cohort]}) (Hobbies: #{@students[i][:hobbies]})".center(80)
     end
     i+=1
   end
 end
 
-def print_footer(students)
-  student_count = students.count
+def print_footer
+  student_count = @students.count
   if student_count == 1
     puts "Overall, we have #{student_count} great student."
   else
@@ -71,28 +77,35 @@ def print_footer(students)
   end
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    students = input_students
+  when "2"
+    show_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students# input the students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)# show the students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
