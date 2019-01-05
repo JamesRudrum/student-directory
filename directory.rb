f73dc3ff9ff7c22246356f10a417aedab6d0a655
James <james.rudrum@hyperisland.se>
2019-01-05 02:50:15 +0000

    load from file

--- directory.rb
+++ directory.rb
@@ -77,6 +77,27 @@ def print_footer
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
@@ -85,17 +106,14 @@ def save_students
   end
   file.close
 end
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "9. Exit"
-end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def process(selection)
@@ -106,17 +124,12 @@ def process(selection)
     show_students
   when "3"
     save_students
+  when "4"
+    load_students
   when "9"
     exit
   else
     puts "I don't know what you meant, try again"
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
   end
 end
 
